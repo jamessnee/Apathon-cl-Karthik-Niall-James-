9003cb299fbad2f0b2fe0211fc462564e0380277
James Snee <james.snee@googlemail.com>
2011-10-08 12:54:17 +0100

    Commiting AppEngine template

---
+++ StatsGame/src/uk/ac/cam/jas250/statsgame/StatsGameServlet.java
@@ -0,0 +1,13 @@
+package uk.ac.cam.jas250.statsgame;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class StatsGameServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
